9451fb7c2e3cf19a2036df9de2cf19eb88194bbc
teekenl <dknight1938@hotmail.com>
2017-09-23 23:07:43 +1000

    Added translation example

--- schema.js
+++ schema.js
@@ -20,6 +20,29 @@ const AuthorType = new GraphQLObjectType({
     })
 });
 
+function translate(lang, str) {
+    // Google Translate API is a paid (but dirt cheap) service. This is my key
+    // and will be disabled by the time the video is out. To generate your own,
+    // go here: https://cloud.google.com/translate/v2/getting_started
+    const apiKey =
+        'AIzaSyBN-bwtos8sKU6X84wkrdjtCF7uzng6kgQ'
+    const url =
+        'https://www.googleapis.com' +
+        '/language/translate/v2' +
+        '?key=' + apiKey +
+        '&source=en' +
+        '&target=' + lang +
+        '&q=' + encodeURIComponent(str)
+    return fetch(url)
+        .then(response => response.json())
+        .then(parsedResponse =>
+            parsedResponse
+                .data
+                .translations[0]
+                .translatedText
+        )
+}
+
 module.exports = new GraphQLSchema({
     query: new GraphQLObjectType({
         name: 'Query',
