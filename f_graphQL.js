fdabe48617159e59adadce8642dca534076bad6c
teekenl <dknight1938@hotmail.com>
2017-09-24 10:53:26 +1000

    Graph QL fetch example fixed.

--- f_graphQL.js
+++ f_graphQL.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const app = require('express');
+const app = express();
 const schema = require('./schema');
+const port = process.env.PORT || 4000;
 
 app.use('/graphql',graphqlHTTP({
    schema,
    graphiql:true
 }));
 
-const server = app.listen(4000, function(err) {
+const server = app.listen(port, function(err) {
     if(err) console.log(err);
-
+    const host = server.address().address;
     console.log(`The server is listening at http://${host}:${port}`);
 });
--- schema.js
+++ schema.js
@@ -1,38 +1,28 @@
 const fetch = require('node-fetch');
 const util = require('util');
-
 const parseXML = util.promisify(require('xml2js').parseString);
 
 const {
     GraphQLSchema,
     GraphQLObjectType,
     GraphQLInt,
-    GraphQLString
+    GraphQLString,
+    GraphQLList
 } = require('graphql');
-
-const AuthorType = new GraphQLObjectType({
-    name: 'Author',
-    description: '...',
-    fields: () => ({
-        name: {
-            type: GraphQLString
-        }
-    })
-});
 
 function translate(lang, str) {
     // Google Translate API is a paid (but dirt cheap) service. This is my key
     // and will be disabled by the time the video is out. To generate your own,
     // go here: https://cloud.google.com/translate/v2/getting_started
     const apiKey =
-        'AIzaSyBN-bwtos8sKU6X84wkrdjtCF7uzng6kgQ'
+        'AIzaSyBN-bwtos8sKU6X84wkrdjtCF7uzng6kgQ';
     const url =
         'https://www.googleapis.com' +
         '/language/translate/v2' +
         '?key=' + apiKey +
         '&source=en' +
         '&target=' + lang +
-        '&q=' + encodeURIComponent(str)
+        '&q=' + encodeURIComponent(str);
     return fetch(url)
         .then(response => response.json())
         .then(parsedResponse =>
@@ -40,8 +30,55 @@ function translate(lang, str) {
                 .data
                 .translations[0]
                 .translatedText
-        )
+        );
 }
+
+// Book params used in GraphQLSchema
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    description: '...',
+    fields: () => ({
+        title: {
+            type: GraphQLString,
+            args: {
+                lang: {type: GraphQLString}
+            },
+            resolve: (xml, args) => {
+                const title = xml.GoodreadsResponse.book[0].title[0];
+                return args.lang ? translate(args.lang,title): title
+            }
+        },
+        isbn: {
+            type: GraphQLString,
+            resolve: xml => xml.GoodreadsResponse.book[0].isbn[0]
+        }
+    })
+});
+
+// Author params used in GraphQLSchema
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    description: '...',
+    fields: () => ({
+        name: {
+            type: GraphQLString,
+            resolve: xml =>
+                xml.GoodreadsResponse.author[0].name[0]
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve: xml => {
+                const ids = xml.GoodreadsResponse.author[0].books[0].book.map(elem => elem.id[0]._)
+                console.log("getting book");
+                return Promise.all(ids.map(id=>
+                    fetch(`https://www.goodreads.com/book/show/${id}.xml?key=42tmzmwXfJJzHcbXlBRg`)
+                        .then(response=>response.json())
+                        .then(parseXML)
+                ))
+            }
+        }
+    })
+});
 
 module.exports = new GraphQLSchema({
     query: new GraphQLObjectType({
@@ -51,15 +88,13 @@ module.exports = new GraphQLSchema({
             author: {
                 type: AuthorType,
                 args: {
-                    id: {type: {GraphQLInt}}
+                    id: { type: GraphQLInt }
                 },
                 resolve: (root, args) => fetch(`https://www.goodreads.com/author/show.xml?id=${args.id}&key=risKm8wwXsIcyEiTktvA`
-                ).then(response=>response.text())
-                    .then(parseXML)
-                    .catch(err=>console.log(err))
-
+                )
+                .then(response=>response.text())
+                .then(parseXML)
             }
         })
     })
-
 });
